95aad7f2218d79e80b8c1c96c7c24fc61ad4d1fa
Paul Zavattieri <paulz92@gmail.com>
2018-01-25 14:30:34 -0500

    cities page, city buttons comp

---
+++ pages/cities.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- insert custom tag into template -->
+  <city-buttons></city-buttons>
+</template>
+
+<script>
+// import city buttons object
+import { CityButtons } from '@/components'
+
+export default {
+  components: {
+    // create custom city buttons tag
+    'city-buttons': CityButtons
+  }
+}
+</script>
